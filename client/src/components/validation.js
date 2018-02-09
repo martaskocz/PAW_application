89cb7c66f1e04a280a3070409032ce3d39523a58
Marta Sobstyl <marta.sobstyl@gmail.com>
2018-02-09 12:29:26 +0100

    create component user registration; add validation

---
+++ client/src/components/validation.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Validation from 'react-validation';
+import { rules, Form, Input, Select, Textarea, Button } from 'react-validation/lib/build/validation.rc'
+import validator from 'validator';
+
+Object.assign(Validation.rules, {
+    required: {
+        rule: value => {
+            return value.toString().trim();
+        },
+        hint: value => {
+            return <span className='form-error is-visible'>Required</span>
+        }
+    },
+    email: {
+        rule: value => {
+            return validator.isEmail(value);
+        },
+        hint: value => {
+            return <span className='form-error is-visible'>{value} isnt an Email.</span>
+        }
+    },
+    alpha: {
+        rule: value => validator.isAlpha(value),
+        hint: () => (
+            <span className="form-error is-visible">
+                String should contain only letters (a-zA-Z).
+            </span>
+        )
+    },
+    number: {
+        rule: value => validator.isInt(value),
+        hint: () => (
+            <span className="form-error is-visible">
+                Only proper number is required
+            </span>
+        )
+    },
+    password: {
+        rule: (value, components) => {
+            const password = components.password.state;
+            const passwordConfirm = components.passwordConfirm.state;
+            const isBothUsed = password
+                && passwordConfirm
+                && password.isUsed
+                && passwordConfirm.isUsed;
+            const isBothChanged = isBothUsed && password.isChanged && passwordConfirm.isChanged;
+            if (!isBothUsed || !isBothChanged) {
+                return true;
+            }
+
+            return password.value === passwordConfirm.value;
+        },
+        hint: () => <span className="form-error is-visible">Passwords should be equal.</span>
+    },
+    api: {
+        hint: value => (
+            <button
+                className="form-error is-visible"
+            >
+                API Error on "{value}" value. Focus to hide.
+            </button>
+        )
+    }
+});
