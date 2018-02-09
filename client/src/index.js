57369a5c3d65baf54f11169f8f0da88589c4c593
Marta Sobstyl <marta.sobstyl@gmail.com>
2018-02-09 12:46:34 +0100

    update routes; add dependecies to main index js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {browserHistory} from 'react-router';
+import Routes from './routes.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import HeaderComponent from './components/header';
+import FooterComponent from './components/footer';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import * as ReactBootstrap from 'react-bootstrap';
+
+
+ReactDOM.render(
+    <div>
+        <HeaderComponent />
+        <Routes history={browserHistory} />
+        <FooterComponent />
+    </div>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
+
 import App from '/.components/app';
 import Error404 from '/.components/error404';
+import Register from './components/user/register';
 
 const Routes = (props) => (
     <Router {...props}>
         <Route path="/" component={App}/>
         <Route path="*" component={Error404}/>
+        <Route server_url="http://localhost:3000" path="/" component={App}/>
+        <Route server_url="http://localhost:3000" path="/Register" component={Register}/>
+        <Route server_url="http://localhost:3000" path="*" component={Error404}/>
     </Router>
 );
 
